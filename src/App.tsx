100ed87030e5e197268494b58490711f6b71ac1f
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-07-29 17:07:19 +0700

    🔥 feat: integrated keycloak auth to client-side

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
-function App() {
+import Keycloak from "keycloak-js";
+import { useEffect, useState } from "react";
+
+const keycloak = new Keycloak({
+	url: import.meta.env.VITE_KEYCLOAK_URL,
+	realm: import.meta.env.VITE_KEYCLOAK_REALM,
+	clientId: import.meta.env.VITE_KEYCLOAK_CLIENT,
+});
+
+const App = () => {
+	const [isLogin, setIsLogin] = useState(false);
+	const [token, setToken] = useState<String>("");
+
+	useEffect(() => {
+		keycloak.init({ onLoad: "check-sso" }).then((auth) => {
+			setIsLogin(auth);
+			setToken(keycloak.token!);
+		});
+	}, []);
+
 	return (
 		<div className="hero bg-base-200 min-h-screen">
 			<div className="hero-content text-center">
@@ -9,11 +28,23 @@ function App() {
 						excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
 						a id nisi.
 					</p>
-					<button className="btn btn-primary">Get Started</button>
+					<button
+						className="btn btn-primary"
+						onClick={() => {
+							if (isLogin) {
+								keycloak.logout();
+							} else {
+								keycloak.login();
+							}
+						}}
+					>
+						{isLogin ? "Sign Out" : "Sign In"}
+					</button>
+					{(console.log(token), null)}
 				</div>
 			</div>
 		</div>
 	);
-}
+};
 
 export default App;
