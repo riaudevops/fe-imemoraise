115946c3f550b11e16f93740c71768afa52ab0a7
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-06 11:01:46 +0700

    💎 style: destructuring landing page component from app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,10 @@
-import Keycloak from "keycloak-js";
-import { useEffect, useState } from "react";
-import Navbar from "./components/landing/Navbar";
-import Hero from "./components/landing/Hero";
-import ProgressTracker from "./components/landing/ProgressTracker";
-import Features from "./components/landing/Features";
-import FAQs from "./components/landing/FAQs";
-import Footer from "./components/landing/Footer";
-
-const keycloak = new Keycloak({
-  url: import.meta.env.VITE_KEYCLOAK_URL,
-  realm: import.meta.env.VITE_KEYCLOAK_REALM,
-  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT,
-});
+import { RouterProvider } from "react-router-dom";
+import router from "./routers/app.routers";
 
 const App = () => {
-  const [isLogin, setIsLogin] = useState(false);
-  // const [token, setToken] = useState<string>("");
-  const [theme, setTheme] = useState<string>("emerald");
-
-  // useEffect(() => {
-  // 	keycloak.init({ onLoad: "check-sso" }).then((auth) => {
-  // 		setIsLogin(auth);
-  // 		setToken(keycloak.token!);
-  // 	});
-  // }, []);
-
-  const handleLogin = () => {
-    if (isLogin) {
-      keycloak.logout();
-    } else {
-      keycloak.login();
-    }
-  };
-
   return (
-    <div data-theme={theme}>
-      <Navbar
-        setTheme={setTheme}
-        currentTheme={theme}
-        isLogin={isLogin}
-        onLoginClick={handleLogin}
-      />
-      <Hero isLogin={isLogin} onLoginClick={handleLogin} />
-      <ProgressTracker />
-      <Features />
-      <FAQs />
-      <Footer isLogin={isLogin} onLoginClick={handleLogin} />
-    </div>
+      <RouterProvider router={router} />
   );
-};
+}
 
 export default App;
