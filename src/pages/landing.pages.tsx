0c53f14f52927477777510926a85afd4f87326d5
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-06 11:09:06 +0700

    🔥 feat: separating each component into pages folder

---
+++ src/pages/landing.pages.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Navbar from "../components/landing/Navbar";
+import Hero from "../components/landing/Hero";
+import ProgressTracker from "../components/landing/ProgressTracker";
+import Features from "../components/landing/Features";
+import FAQs from "../components/landing/FAQs";
+import Footer from "../components/landing/Footer";
+
+import { useKeycloak } from "../hooks/useKeycloak.hooks";
+
+const LandingPages = () => {
+	const [theme, setTheme] = useState<string>("emerald");
+
+	const { keycloak, isLogin, token } = useKeycloak();
+	console.log(token);
+
+	const handleLogin = () => {
+		if (isLogin) {
+			keycloak.logout();
+		} else {
+			keycloak.login();
+		}
+	};
+
+	return (
+		<div data-theme={theme}>
+			<Navbar
+				setTheme={setTheme}
+				currentTheme={theme}
+				isLogin={isLogin}
+				onLoginClick={handleLogin}
+			/>
+			<Hero isLogin={isLogin} onLoginClick={handleLogin} />
+			<ProgressTracker />
+			<Features />
+			<FAQs />
+			<Footer isLogin={isLogin} onLoginClick={handleLogin} />
+		</div>
+	);
+};
+
+export default LandingPages;
