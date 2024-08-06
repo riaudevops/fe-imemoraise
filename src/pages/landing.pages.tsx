b8df88e165fb8a59253b5b6c6d67d896508aaa50
FaruqHZ <faruqhz0113@gmail.com>
2024-08-06 14:59:13 +0700

    🎯 chore: removing unused props on footer component

--- src/pages/landing.pages.tsx
+++ src/pages/landing.pages.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Navbar from "../components/landing/Navbar";
 import Hero from "../components/landing/Hero";
-import ProgressTracker from "../components/landing/ProgressTracker";
 import Features from "../components/landing/Features";
 import FAQs from "../components/landing/FAQs";
 import Footer from "../components/landing/Footer";
@@ -9,34 +8,33 @@ import Footer from "../components/landing/Footer";
 import { useKeycloak } from "../hooks/useKeycloak.hooks";
 
 const LandingPages = () => {
-	const [theme, setTheme] = useState<string>("emerald");
+  const [theme, setTheme] = useState<string>("emerald");
 
-	const { keycloak, isLogin, token } = useKeycloak();
-	console.log(token);
+  const { keycloak, isLogin, token } = useKeycloak();
+  console.log(token);
 
-	const handleLogin = () => {
-		if (isLogin) {
-			keycloak.logout();
-		} else {
-			keycloak.login();
-		}
-	};
+  const handleLogin = () => {
+    if (isLogin) {
+      keycloak.logout();
+    } else {
+      keycloak.login();
+    }
+  };
 
-	return (
-		<div data-theme={theme}>
-			<Navbar
-				setTheme={setTheme}
-				currentTheme={theme}
-				isLogin={isLogin}
-				onLoginClick={handleLogin}
-			/>
-			<Hero isLogin={isLogin} onLoginClick={handleLogin} />
-			{/* <ProgressTracker /> */}
-			<Features />
-			<FAQs />
-			<Footer isLogin={isLogin} onLoginClick={handleLogin} />
-		</div>
-	);
+  return (
+    <div data-theme={theme}>
+      <Navbar
+        setTheme={setTheme}
+        currentTheme={theme}
+        isLogin={isLogin}
+        onLoginClick={handleLogin}
+      />
+      <Hero isLogin={isLogin} onLoginClick={handleLogin} />
+      <Features />
+      <FAQs />
+      <Footer />
+    </div>
+  );
 };
 
 export default LandingPages;
