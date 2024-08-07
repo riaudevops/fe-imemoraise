c98c6c226782b20a759814e06eb5e9eebc9adb76
rel1vee <muhzakierbaisyas@gmail.com>
2024-08-07 16:45:12 +0700

    🔥 feat: adding setoran component for dashboard mahasiswa

---
+++ src/components/dashboard/mahasiswa/SetoranMahasiswa.tsx
@@ -0,0 +1,16 @@
+import { ThemeProps } from "../../../types/common.types";
+import { Layout, sidebarMahasiswaItems } from "../Layout";
+
+const SetoranMahasiswa = ({ setTheme, currentTheme }: ThemeProps) => {
+  return (
+    <Layout
+      setTheme={setTheme}
+      currentTheme={currentTheme}
+      sidebarItems={sidebarMahasiswaItems}
+    >
+      <h1 className="text-2xl font-bold">Setoran</h1>
+    </Layout>
+  );
+};
+
+export default SetoranMahasiswa;
