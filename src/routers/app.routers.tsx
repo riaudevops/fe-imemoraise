bc1a7fa08f82cebfadf1609eecabb13e238c8e0c
rel1vee <muhzakierbaisyas@gmail.com>
2024-08-07 16:55:03 +0700

    🔥 feat: adding new route for each pages created

--- src/routers/app.routers.tsx
+++ src/routers/app.routers.tsx
@@ -1,10 +1,30 @@
-import LandingPages from "../pages/landing.pages.tsx";
 import { createBrowserRouter } from "react-router-dom";
+import LandingPages from "../pages/landing.pages.tsx";
+import DashboardMahasiswaPages from "../pages/mahasiswa/dashboard.mahasiswa.pages.tsx";
+import SetoranMahasiswaPages from "../pages/mahasiswa/setoran.mahasiswa.pages.tsx";
+import DashboardPAPages from "../pages/pa/dashboard.pa.pages.tsx";
+import MahasiswaPAPages from "../pages/pa/mahasiswa.pa.pages.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPages />,
+  },
+  {
+    path: "/mahasiswa",
+    element: <DashboardMahasiswaPages />,
+  },
+  {
+    path: "/mahasiswa/setoran",
+    element: <SetoranMahasiswaPages />,
+  },
+  {
+    path: "/pa",
+    element: <DashboardPAPages />,
+  },
+  {
+    path: "/pa/mahasiswa",
+    element: <MahasiswaPAPages />,
   },
 ]);
 
