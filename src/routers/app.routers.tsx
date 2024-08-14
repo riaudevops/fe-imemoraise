b1353b9ea99dace675fcd4c0d986e8ee3ab5d63c
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-14 12:59:22 +0700

    🔥 feat: implements protected routes based on roles at all defined routers

--- src/routers/app.routers.tsx
+++ src/routers/app.routers.tsx
@@ -4,28 +4,49 @@ import DashboardMahasiswaPages from "../pages/mahasiswa/dashboard.mahasiswa.page
 import SetoranMahasiswaPages from "../pages/mahasiswa/setoran.mahasiswa.pages.tsx";
 import DashboardPAPages from "../pages/pa/dashboard.pa.pages.tsx";
 import MahasiswaPAPages from "../pages/pa/mahasiswa.pa.pages.tsx";
+import ProtectedRoute from "./protected.routers.tsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPages />,
-  },
-  {
-    path: "/mahasiswa",
-    element: <DashboardMahasiswaPages />,
-  },
-  {
-    path: "/mahasiswa/setoran",
-    element: <SetoranMahasiswaPages />,
-  },
-  {
-    path: "/pa",
-    element: <DashboardPAPages />,
-  },
-  {
-    path: "/pa/mahasiswa",
-    element: <MahasiswaPAPages />,
-  },
+	{
+		path: "/",
+		element: <LandingPages />,
+	},
+	{
+		path: "/unauthorized",
+		element: <p>Siapa Ente...</p>,
+	},
+	{
+		path: "/mahasiswa",
+		element: (
+			<ProtectedRoute roles={["mahasiswa"]}>
+				<DashboardMahasiswaPages />
+			</ProtectedRoute>
+		),
+	},
+	{
+		path: "/mahasiswa/setoran",
+		element: (
+			<ProtectedRoute roles={["mahasiswa"]}>
+				<SetoranMahasiswaPages />
+			</ProtectedRoute>
+		),
+	},
+	{
+		path: "/pa",
+		element: (
+			<ProtectedRoute roles={["dosen-pa"]}>
+				<DashboardPAPages />
+			</ProtectedRoute>
+		),
+	},
+	{
+		path: "/pa/mahasiswa",
+		element: (
+			<ProtectedRoute roles={["dosen-pa"]}>
+				<MahasiswaPAPages />
+			</ProtectedRoute>
+		),
+	},
 ]);
 
 export default router;
