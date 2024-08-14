60f62a15ca910a3564cf9ed5a363acf0fb1108d1
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-14 12:12:31 +0700

    ⚙ refactor: changing keycloak integration at fe for avoiding clone keycloak object init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { ReactKeycloakProvider } from "@react-keycloak/web";
 import router from "./routers/app.routers";
 import { RouterProvider } from "react-router-dom";
+import Keycloak from "keycloak-js";
+
+const keycloak = new Keycloak({
+	url: import.meta.env.VITE_KEYCLOAK_URL,
+	realm: import.meta.env.VITE_KEYCLOAK_REALM,
+	clientId: import.meta.env.VITE_KEYCLOAK_CLIENT,
+});
 
 const App = () => {
-  return <RouterProvider router={router} />;
+	return (
+		<ReactKeycloakProvider authClient={keycloak}>
+			<RouterProvider router={router} />
+		</ReactKeycloakProvider>	
+	);
 };
 
 export default App;
