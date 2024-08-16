a95997053a4ba5a0f717830ffb461c60b1cfe52e
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-16 16:48:18 +0700

    🔥 feat: implements loading in hero when keycloak isn't being initialized yet

--- src/pages/landing.pages.tsx
+++ src/pages/landing.pages.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Navbar from "../components/landing/Navbar";
 import Hero from "../components/landing/Hero";
 import Features from "../components/landing/Features";
@@ -6,10 +7,21 @@ import Footer from "../components/landing/Footer";
 import { useTheme } from "../hooks/useTheme.hooks";
 import { NavigateFunction, useNavigate } from "react-router-dom";
 import { useKeycloak } from "@react-keycloak/web";
+import { LoadingInterfaceLanding } from "../components/LoadingInterface";
 
 const LandingPages = () => {
   const { theme, setTheme } = useTheme();
-  const { keycloak } = useKeycloak();
+  const { keycloak, initialized } = useKeycloak(); // initialized is provided by useKeycloak hook to check if Keycloak is ready
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (initialized) {
+      setIsLoading(false);
+    }
+  }, [initialized]);
 
   const handleLogin = () => {
     if (keycloak.authenticated) {
@@ -19,17 +31,19 @@ const LandingPages = () => {
     }
   };
 
-  const navigate = useNavigate();
-
   const handleGoToDashboard = (navigate: NavigateFunction) => {
-  
     if (keycloak.hasResourceRole("mahasiswa")) {
       navigate("/mahasiswa");
     } else if (keycloak.hasResourceRole("dosen-pa")) {
       navigate("/pa");
     }
   };
-  
+
+  // Render loading screen if still initializing
+  if (isLoading) {
+    return <LoadingInterfaceLanding />;
+  }
+
   return (
     <div data-theme={theme}>
       <Navbar
@@ -39,7 +53,11 @@ const LandingPages = () => {
         onLoginClick={handleLogin}
         onGoToDashboardClick={() => handleGoToDashboard(navigate)}
       />
-      <Hero isLogin={keycloak.authenticated!} onLoginClick={handleLogin} onGoToDashboardClick={() => handleGoToDashboard(navigate)} />
+      <Hero
+        isLogin={keycloak.authenticated!}
+        onLoginClick={handleLogin}
+        onGoToDashboardClick={() => handleGoToDashboard(navigate)}
+      />
       <Features />
       <FAQs />
       <Footer />
