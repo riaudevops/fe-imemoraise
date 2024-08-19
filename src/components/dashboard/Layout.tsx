82cdbf133dba336af71521ad897b82ced7855a3c
rel1vee <muhzakierbaisyas@gmail.com>
2024-08-19 08:32:25 +0700

    🎯 chore: separating interface

--- src/components/dashboard/Layout.tsx
+++ src/components/dashboard/Layout.tsx
@@ -1,18 +1,7 @@
-import { ReactNode } from "react";
-import ThemeSwitcher from "../ThemeSwitcher";
+import { LayoutProps } from "../../interfaces/common.interfaces";
 import { Link, useLocation } from "react-router-dom";
-import { ThemeProps } from "../../types/common.types";
 import { useKeycloak } from "@react-keycloak/web";
-
-interface LayoutProps extends ThemeProps {
-  children: ReactNode;
-  sidebarItems: Array<{
-    icon: ReactNode;
-    label: string;
-    link: string;
-  }>;
-  subpageTitle?: string;
-}
+import ThemeSwitcher from "../ThemeSwitcher";
 
 const Layout = ({
   setTheme,
@@ -25,10 +14,10 @@ const Layout = ({
   const { keycloak } = useKeycloak();
 
   return (
-    <div className="drawer lg:drawer-open h-screen">
+    <div className="h-full drawer lg:drawer-open">
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content h-full">
-        <div className="pl-4 h-16 shadow-md navbar bg-base-100 border-b border-neutral-content flex justify-between sticky top-0 z-50">
+      <div className="h-full drawer-content">
+        <div className="sticky top-0 z-10 flex justify-between h-16 pl-4 border-b shadow-md navbar bg-base-100 border-neutral-content">
           <div className="flex gap-1">
             <label
               htmlFor="my-drawer"
@@ -38,7 +27,7 @@ const Layout = ({
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                className="inline-block w-5 h-5 stroke-current"
+                className="inline-block h-5 stroke-current"
               >
                 <path
                   strokeLinecap="round"
@@ -52,11 +41,14 @@ const Layout = ({
           </div>
           <div>
             <ThemeSwitcher setTheme={setTheme} currentTheme={currentTheme} />
-            <button className="btn btn-ghost text-primary" onClick={() => keycloak.logout()}>
+            <button
+              className="btn btn-ghost text-primary"
+              onClick={() => keycloak.logout()}
+            >
               <span className="hidden sm:inline">Sign Out</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-5 h-5"
+                className="h-5"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -71,29 +63,31 @@ const Layout = ({
             </button>
           </div>
         </div>
-
         <div className="p-4 bg-base-200/20">{children}</div>
       </div>
-      <div className="drawer-side z-50">
+      <div className="z-20 drawer-side">
         <label htmlFor="my-drawer" className="drawer-overlay"></label>
-        <ul className="h-full p-4 w-80 menu bg-base-100 border-r border-black">
+        <ul className="h-full p-4 border-r border-black w-80 menu bg-base-100">
           <div className="flex items-center justify-center h-24 mb-3 text-base-100/95">
             <img
               src="/uin-suska.svg"
               alt="UIN Suska Riau"
-              className="w-16 h-16 mr-2"
+              className="h-16 mr-2"
             />
             <span className="text-2xl font-bold text-base-content">
-              <span className="underline italic">iMemoraise</span> <br /> UIN Suska Riau
+              <span className="italic underline">iMemoraise</span> <br /> UIN
+              Suska Riau
             </span>
           </div>
-          <div className="mx-auto h-[1px] w-48 mb-5 bg-base-content"/>
+          <div className="mx-auto h-[1px] w-48 mb-5 bg-base-content" />
           {sidebarItems.map((item, index) => (
             <li key={index}>
               <Link
                 to={item.link}
                 className={`m-2 text-base hover:bg-primary/10 ${
-                  location.pathname === item.link || (location.pathname === "/pa/mahasiswa/setoran" && item.label === "Mahasiswa")
+                  location.pathname === item.link ||
+                  (location.pathname === "/dosen-pa/mahasiswa/setoran" &&
+                    item.label === "Mahasiswa")
                     ? "bg-primary/40"
                     : "bg-base-200"
                 }`}
@@ -109,70 +103,4 @@ const Layout = ({
   );
 };
 
-const sidebarMahasiswaItems = [
-  {
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="w-5 h-5 mx-2"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-      </svg>
-    ),
-    label: "Dashboard",
-    link: "/mahasiswa",
-  },
-  {
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="w-5 h-5 mx-2"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fillRule="evenodd"
-          d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-    label: "Setoran",
-    link: "/mahasiswa/setoran",
-  },
-];
-
-const sidebarPAItems = [
-  {
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="w-5 h-5 mx-2"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-      </svg>
-    ),
-    label: "Dashboard",
-    link: "/pa",
-  },
-  {
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="w-5 h-5 mx-2"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
-      </svg>
-    ),
-    label: "Mahasiswa",
-    link: "/pa/mahasiswa",
-  },
-];
-
-export { Layout, sidebarPAItems, sidebarMahasiswaItems };
+export default Layout;
