944a2c943ff8fee35ca0c4fbd9666d4561136632
rel1vee <muhzakierbaisyas@gmail.com>
2024-08-19 08:33:39 +0700

    🎯 chore: separating layout

--- src/components/dashboard/pa/DashboardPA.tsx
+++ src/components/dashboard/pa/DashboardPA.tsx
@@ -1,16 +1,8 @@
-import { Layout, sidebarPAItems } from "../Layout";
-import { ThemeProps } from "../../../types/common.types";
-
-const DashboardPA = ({ setTheme, currentTheme }: ThemeProps) => {
+const DashboardPA = () => {
   return (
-    <Layout
-      setTheme={setTheme}
-      currentTheme={currentTheme}
-      sidebarItems={sidebarPAItems}
-      subpageTitle="Dashboard PA"
-    >
-      <h1 className="text-2xl font-bold">Dashboard PA</h1>
-    </Layout>
+    <>
+      <p>Apa yaa isinya...</p>
+    </>
   );
 };
 
