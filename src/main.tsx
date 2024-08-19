b19435940565d11d993d651a83b5fdb9e4d1e8a5
rel1vee <muhzakierbaisyas@gmail.com>
2024-08-19 08:28:41 +0700

    💎 style: formatting code

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import "./index.css";
-import App from "./App";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <div>
+  <>
     <App />
-  </div>
+  </>
 );
