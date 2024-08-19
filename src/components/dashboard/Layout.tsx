fea39c6362968022f7580a9d0b7ecfe58807a264
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-19 15:01:16 +0700

    🔥 feat: adding back to history btn in nav

--- src/components/dashboard/Layout.tsx
+++ src/components/dashboard/Layout.tsx
@@ -37,7 +37,12 @@ const Layout = ({
                 ></path>
               </svg>
             </label>
-            <span className="text-lg font-semibold">{subpageTitle}</span>
+            <span className="text-lg font-semibold">
+              <button className="btn btn-rounded-sm btn-ghost text-error px-2 p-0 mr-1" onClick={() => window.history.back()}>
+                <span className="underline">{"< Kembali"}</span>
+              </button>
+              {subpageTitle}
+            </span>
           </div>
           <div>
             <ThemeSwitcher setTheme={setTheme} currentTheme={currentTheme} />
