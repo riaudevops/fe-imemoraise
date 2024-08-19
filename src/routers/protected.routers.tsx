7e61254bfa57941f1971179d4cda5f7855395832
rel1vee <muhzakierbaisyas@gmail.com>
2024-08-19 08:38:42 +0700

    🎯 chore: separating interface

--- src/routers/protected.routers.tsx
+++ src/routers/protected.routers.tsx
@@ -1,34 +1,30 @@
+import { ProtectedRouteProps } from "../interfaces/common.interfaces";
 import { useKeycloak } from "@react-keycloak/web";
-import { Navigate } from "react-router-dom";
 import { useTheme } from "../hooks/useTheme.hooks";
-
-interface ProtectedRouteProps {
-    children: React.ReactNode;
-    roles: string[];
-}
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children, roles }: ProtectedRouteProps) => {
-    const { keycloak, initialized } = useKeycloak();
-    const { theme } = useTheme();
+  const { keycloak, initialized } = useKeycloak();
+  const { theme } = useTheme();
 
-    // Tampilkan loading atau null hingga Keycloak siap
-    if (!initialized) {
-        return <div data-theme={theme} className="w-screen h-screen" />;
-    }
+  // Tampilkan loading atau null hingga Keycloak siap
+  if (!initialized) {
+    return <div data-theme={theme} className="w-screen h-screen" />;
+  }
 
-    // Periksa apakah pengguna sudah terautentikasi
-    if (!keycloak.authenticated) {
-        return <Navigate to="/" />;
-    }
+  // Periksa apakah pengguna sudah terautentikasi
+  if (!keycloak.authenticated) {
+    return <Navigate to="/" />;
+  }
 
-    // Periksa apakah pengguna memiliki salah satu dari peran yang diizinkan
-    const hasRole = roles.some((role) => keycloak.hasResourceRole(role));
+  // Periksa apakah pengguna memiliki salah satu dari peran yang diizinkan
+  const hasRole = roles.some((role) => keycloak.hasResourceRole(role));
 
-    if (!hasRole) {
-        return <Navigate to="/unauthorized" />;
-    }
+  if (!hasRole) {
+    return <Navigate to="/unauthorized" />;
+  }
 
-    return children;
+  return children;
 };
 
 export default ProtectedRoute;
