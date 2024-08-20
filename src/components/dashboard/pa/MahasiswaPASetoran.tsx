bae0c4d0a544a84353c2fdb0bd21708e5bf7be32
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-20 17:17:04 +0700

    ✨ perf: implements extra sanitize layer input in search query

--- src/components/dashboard/pa/MahasiswaPASetoran.tsx
+++ src/components/dashboard/pa/MahasiswaPASetoran.tsx
@@ -2,297 +2,309 @@ import { useLocation } from "react-router-dom";
 import useAxiosInstance from "../../../configs/axios.configs";
 import { useEffect, useState } from "react";
 import {
-  dataSetoranMahasiswaPAProps,
-  selectedDataForModalBoxInfoPAProps,
+	dataSetoranMahasiswaPAProps,
+	selectedDataForModalBoxInfoPAProps,
 } from "../../../interfaces/common.interfaces";
 import { formatDateTime, labelPersyaratan } from "../Constant";
+import DOMPurify from "dompurify";
 
 const MahasiswaPASetoran = () => {
-  const location = useLocation();
-  const axiosInstance = useAxiosInstance();
-  const [nama, setNama] = useState("");
-  const [nim, setNim] = useState("");
-  const [nip, setNip] = useState("");
-  const [showModal, setShowModal] = useState(false);
-  const [isChecked, setIsChecked] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [setoranDate, setSetoranDate] = useState<Date>(new Date());
+	const location = useLocation();
+	const axiosInstance = useAxiosInstance();
+	const [nama, setNama] = useState("");
+	const [nim, setNim] = useState("");
+	const [nip, setNip] = useState("");
+	const [showModal, setShowModal] = useState(false);
+	const [isChecked, setIsChecked] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [setoranDate, setSetoranDate] = useState<Date>(new Date());
 
-  const [alertInfo, setAlertInfo] = useState<{
-    type: "success" | "error";
-    message: string;
-  } | null>();
+	const [alertInfo, setAlertInfo] = useState<{
+		type: "success" | "error";
+		message: string;
+	} | null>();
 
-  const [dataSetoranMahasiswa, setDataSetoranMahasiswa] =
-    useState<dataSetoranMahasiswaPAProps[]>();
+	const [dataSetoranMahasiswa, setDataSetoranMahasiswa] =
+		useState<dataSetoranMahasiswaPAProps[]>();
 
-  const [selectedData, setSelectedData] =
-    useState<selectedDataForModalBoxInfoPAProps>();
+	const [selectedData, setSelectedData] =
+		useState<selectedDataForModalBoxInfoPAProps>();
 
-  const handleRowClick = (data: selectedDataForModalBoxInfoPAProps) => {
-    setSelectedData(data);
-    setShowModal(true);
-  };
+	const handleRowClick = (data: selectedDataForModalBoxInfoPAProps) => {
+		setSelectedData(data);
+		setShowModal(true);
+	};
 
-  const fetchDataSetoranMahasiswa = () => {
-    axiosInstance
-      .get(`/mahasiswa/surah/${nim}`)
-      .then((res) => res.data.data)
-      .then((res) => {
-        setDataSetoranMahasiswa(res);
-      });
-  };
+	const fetchDataSetoranMahasiswa = () => {
+		axiosInstance
+			.get(`/mahasiswa/setoran/${nim}`)
+			.then((res) => res.data.data)
+			.then((res) => {
+				setDataSetoranMahasiswa(res);
+			});
+	};
 
-  useEffect(() => {
-    const queryParams = new URLSearchParams(location.search);
-    const namaParam = queryParams.get("nama");
-    const nimParam = queryParams.get("nim");
-    const nipParam = queryParams.get("nip");
+	useEffect(() => {
+		const queryParams = new URLSearchParams(location.search);
+		const namaParam = DOMPurify.sanitize(queryParams.get("nama") || "");
+		const nimParam = DOMPurify.sanitize(queryParams.get("nim") || "");
+		const nipParam = DOMPurify.sanitize(queryParams.get("nip") || "");
 
-    if (namaParam) setNama(namaParam);
-    if (nimParam) setNim(nimParam);
-    if (nipParam) setNip(nipParam);
-  }, [location.search]);
+		if (namaParam) setNama(namaParam);
+		if (nimParam) setNim(nimParam);
+		if (nipParam) setNip(nipParam);
+	}, [location.search]);
 
-  useEffect(() => {
-    fetchDataSetoranMahasiswa();
-  }, [axiosInstance, nim]);
+	useEffect(() => {
+		fetchDataSetoranMahasiswa();
+	}, [axiosInstance, nim]);
 
-  useEffect(() => {
-    if (alertInfo) {
-      const timer = setTimeout(() => {
-        setAlertInfo(null);
-      }, 5000); // Alert will disappear after 5 seconds
+	useEffect(() => {
+		if (alertInfo) {
+			const timer = setTimeout(() => {
+				setAlertInfo(null);
+			}, 5000); // Alert will disappear after 5 seconds
 
-      return () => clearTimeout(timer);
-    }
-  }, [alertInfo]);
+			return () => clearTimeout(timer);
+		}
+	}, [alertInfo]);
 
-  const handleValidation = () => {
-    setIsLoading(true);
-    axiosInstance
-      .post(`/dosen/mahasiswa`, {
-        nim: nim,
-        nip: nip,
-        nomor_surah: selectedData?.nomor_surah,
-        tgl_setoran: setoranDate,
-      })
-      .then(() => {
-        setShowModal(false);
-        setIsChecked(false);
-        setAlertInfo({ type: "success", message: "Yeay, proses validasi setoran berhasil! ✨" });
-        fetchDataSetoranMahasiswa();
-      })
-      .catch((error) => {
-        setAlertInfo({
-          type: "error",
-          message: "Validasi gagal: " + error.message,
-        });
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
+	const handleValidation = () => {
+		setIsLoading(true);
+		axiosInstance
+			.post(`/dosen/mahasiswa`, {
+				nim: nim,
+				nip: nip,
+				nomor_surah: selectedData?.nomor_surah,
+				tgl_setoran: setoranDate,
+			})
+			.then((res) => res.data)
+			.then((res) => {
+				setShowModal(false);
+				setIsChecked(false);
+				setAlertInfo({ type: "success", message: res.message });
+				fetchDataSetoranMahasiswa();
+			})
+			.catch((error) => {
+				setAlertInfo({
+					type: "error",
+					message: "Validasi gagal: " + error.response.data.message,
+				});
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
+	};
 
-  return (
-    <div className="flex flex-col gap-6 mt-2">
-      {alertInfo && (
-        <div className="z-20 top-5 toast toast-top">
-          <div
-            className={`alert ${
-              alertInfo.type === "success" ? "alert-success" : "alert-error"
-            }`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6 stroke-current shrink-0"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span className="-ml-2 font-semibold">{alertInfo.message}</span>
-          </div>
-        </div>
-      )}
+	return (
+		<div className="flex flex-col gap-6 mt-2">
+			{alertInfo && (
+				<div className="top-5 toast toast-top" style={{ zIndex: 9999 }}>
+					<div
+						className={`alert ${
+							alertInfo.type === "success" ? "alert-success" : "alert-error"
+						}`}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="w-6 h-6 stroke-current shrink-0"
+							fill="none"
+							viewBox="0 0 24 24"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+						<span className="-ml-2 font-semibold">{alertInfo.message}</span>
+					</div>
+				</div>
+			)}
 
-      <div className="flex justify-between h-12">
-        <div className="font-semibold">
-          <div className="flex">
-            <span className="w-16">Nama</span>
-            <span>: {nama}</span>
-          </div>
-          <div className="flex">
-            <span className="w-16">NIM</span>
-            <span>: {nim}</span>
-          </div>
-        </div>
-        <button className="text-lg font-semibold rounded-sm w-28 btn btn-outline btn-rounded-sm btn-primary">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="h-5"
-          >
-            <path d="M6 9V2h12v7" />
-            <path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2" />
-            <rect x="6" y="14" width="12" height="8" />
-            <line x1="8" y1="17" x2="16" y2="17" />
-            <line x1="8" y1="20" x2="16" y2="20" />
-          </svg>
-          Cetak
-        </button>
-      </div>
+			<div className="flex justify-between h-12">
+				<div className="font-semibold">
+					<div className="flex">
+						<span className="w-16">Nama</span>
+						<span>: {nama}</span>
+					</div>
+					<div className="flex">
+						<span className="w-16">NIM</span>
+						<span>: {nim}</span>
+					</div>
+				</div>
+				<button className="text-lg font-semibold rounded-sm w-28 btn btn-outline btn-rounded-sm btn-primary">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						className="h-5"
+					>
+						<path d="M6 9V2h12v7" />
+						<path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2" />
+						<rect x="6" y="14" width="12" height="8" />
+						<line x1="8" y1="17" x2="16" y2="17" />
+						<line x1="8" y1="20" x2="16" y2="20" />
+					</svg>
+					Cetak
+				</button>
+			</div>
 
-      {/* Table Setoran */}
-      <div className="rounded-xl overflow-auto flex-grow h-[calc(100vh-176px)]">
-        <table className="table text-sm text-center table-auto">
-          <thead className="sticky top-0 text-sm font-bold rounded-md bg-primary text-base-100">
-            <tr>
-              <th>No.</th>
-              <th>Nama Surah</th>
-              <th>Tanggal Setoran Hafalan</th>
-              <th>Persyaratan Setoran</th>
-              <th>Dosen yang Mengesahkan</th>
-              <th>Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            {dataSetoranMahasiswa?.map((data, index) => (
-              <tr
-                key={index}
-                className="text-neutral-content odd:bg-base-200 even:bg-base-300 hover:bg-base-100 active:bg-base-300"
-              >
-                <th className="text-base-content">
-                  {data.setoran?.length ? "✔" : ""} {index + 1}.
-                </th>
-                <td className="font-bold text-base-content">{data.nama}</td>
-                <td className="italic underline text-base-content">
-                  {formatDateTime(data.setoran?.[0]?.tgl_setoran || "")}
-                </td>
-                <td>
-                  <div
-                    className={` ${
-                      labelPersyaratan(data.label)[0]
-                    } py-2 rounded-sm`}
-                  >
-                    <p className="font-semibold text-base-content">
-                      {labelPersyaratan(data.label)[1]}
-                    </p>
-                  </div>
-                </td>
-                <td className="italic underline text-base-content">
-                  {data.setoran?.[0]?.dosen.nama}
-                </td>
-                <td>
-                  <button
-                    onClick={() =>
-                      handleRowClick({
-                        nama: nama,
-                        nim: nim,
-                        surah: data.nama,
-                        nomor_surah: data.nomor,
-                      })
-                    }
-                    className={`btn ${
-                      data.setoran?.length && "btn-disabled"
-                    } btn-sm btn-base-100 rounded-sm btn-outline`}
-                  >
-                    ACC
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+			{/* Table Setoran */}
+			<div className="rounded-xl overflow-auto flex-grow h-[calc(100vh-176px)]">
+				<table className="table text-sm text-center table-auto">
+					<thead className="sticky top-0 text-sm font-bold rounded-md bg-primary text-base-100">
+						<tr>
+							<th>No.</th>
+							<th>Nama Surah</th>
+							<th>Tanggal Setoran Hafalan</th>
+							<th>Persyaratan Setoran</th>
+							<th>Dosen yang Mengesahkan</th>
+							<th>Aksi</th>
+						</tr>
+					</thead>
+					<tbody>
+						{dataSetoranMahasiswa?.map((data, index) => (
+							<tr
+								key={index}
+								className="text-neutral-content odd:bg-base-200 even:bg-base-300 hover:bg-base-100 active:bg-base-300"
+							>
+								<th className="text-base-content">
+									{data.setoran?.length ? "✔" : ""} {index + 1}.
+								</th>
+								<td className="font-bold text-base-content">{data.nama}</td>
+								<td className="italic underline text-base-content">
+									{formatDateTime(data.setoran?.[0]?.tgl_setoran || "")}
+								</td>
+								<td>
+									<div
+										className={` ${
+											labelPersyaratan(data.label)[0]
+										} py-2 rounded-sm`}
+									>
+										<p className="font-semibold text-base-content">
+											{labelPersyaratan(data.label)[1]}
+										</p>
+									</div>
+								</td>
+								<td className="italic underline text-base-content">
+									{data.setoran?.[0]?.dosen.nama}
+								</td>
+								<td>
+									<button
+										onClick={() =>
+											handleRowClick({
+												nama: nama,
+												nim: nim,
+												surah: data.nama,
+												nomor_surah: data.nomor,
+											})
+										}
+										className={`btn ${
+											data.setoran?.length && "btn-disabled"
+										} btn-sm btn-base-100 rounded-sm btn-outline`}
+									>
+										ACC
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 
-      {showModal && (
-        <div className="flex justify-center items-center bg-black bg-opacity-50 w-screen h-screen fixed top-0 left-0 z-50">
-          <div className="modal-box lg:ml-10">
-            <h2 className="mb-6 text-xl font-bold text-center">
-              Validasi Pengesahan Anda:
-            </h2>
+			{showModal && (
+				<div className="flex justify-center items-center bg-black bg-opacity-50 w-screen h-screen fixed top-0 left-0 z-50">
+					<div className="modal-box lg:ml-10">
+						<h2 className="mb-6 text-xl font-bold text-center">
+							Validasi Pengesahan Anda:
+						</h2>
 
-            <div className="w-full p-2 mt-3 font-medium bg-warning/20">
-              <p className="text-lg text-center">Nama Mahasiswa</p>
-            </div>
-            <div className="w-full p-2 bg-base-300">
-              <p className="text-lg text-center">{selectedData?.nama}</p>
-            </div>
-            <div className="w-full p-2 mt-3 font-medium bg-warning/20">
-              <p className="text-lg text-center">NIM</p>
-            </div>
-            <div className="w-full p-2 bg-base-300">
-              <p className="text-lg text-center">{selectedData?.nim}</p>
-            </div>
-            <div className="w-full p-2 mt-3 font-medium bg-warning/20">
-              <p className="text-lg text-center">Nama Surah</p>
-            </div>
-            <div className="w-full p-2 bg-base-300">
-              <p className="text-lg text-center">{selectedData?.surah}</p>
-            </div>
-            <div className="w-full p-2 mt-3 font-medium bg-warning/20">
-              <p className="text-lg text-center">Tanggal Setoran Hafalan (opsional)</p>
-            </div>
-            <div className="w-full p-2 text-center bg-base-300 flex flex-col gap-3">
-              <input type="date" className="input input-bordered" onChange={(e) => setSetoranDate(new Date(e.target.value))} />
-              <span><span className="font-bold">notes:</span> jika tanggal setoran tidak ditentukan secara manual, akan di tetapkan sesuai tanggal hari ini secara default.</span>
-            </div>
+						<div className="w-full p-2 mt-3 font-medium bg-warning/20">
+							<p className="text-lg text-center">Nama Mahasiswa</p>
+						</div>
+						<div className="w-full p-2 bg-base-300">
+							<p className="text-lg text-center">{selectedData?.nama}</p>
+						</div>
+						<div className="w-full p-2 mt-3 font-medium bg-warning/20">
+							<p className="text-lg text-center">NIM</p>
+						</div>
+						<div className="w-full p-2 bg-base-300">
+							<p className="text-lg text-center">{selectedData?.nim}</p>
+						</div>
+						<div className="w-full p-2 mt-3 font-medium bg-warning/20">
+							<p className="text-lg text-center">Nama Surah</p>
+						</div>
+						<div className="w-full p-2 bg-base-300">
+							<p className="text-lg text-center">{selectedData?.surah}</p>
+						</div>
+						<div className="w-full p-2 mt-3 font-medium bg-warning/20">
+							<p className="text-lg text-center">
+								Tanggal Setoran Hafalan (opsional)
+							</p>
+						</div>
+						<div className="w-full p-2 text-center bg-base-300 flex flex-col gap-3">
+							<input
+								type="date"
+								className="input input-bordered"
+								onChange={(e) => setSetoranDate(new Date(e.target.value))}
+							/>
+							<span>
+								<span className="font-bold">notes:</span> jika tanggal setoran
+								tidak ditentukan secara manual, akan di tetapkan sesuai tanggal
+								hari ini secara default.
+							</span>
+						</div>
 
-            <div className="flex justify-start gap-3 mt-6">
-              <input
-                type="checkbox"
-                onChange={() => setIsChecked(!isChecked)}
-                className="checkbox border-orange-400 [--chkbg:theme(colors.indigo.600)] [--chkfg:orange] checked:border-indigo-800"
-              />
-              <p>
-                Saya yakin untuk{" "}
-                <span className="italic font-bold">
-                  memvalidasi hafalan surat
-                </span>{" "}
-                mahasiswa tersebut.
-              </p>
-            </div>
+						<div className="flex justify-start gap-3 mt-6">
+							<input
+								type="checkbox"
+								onChange={() => setIsChecked(!isChecked)}
+								className="checkbox border-orange-400 [--chkbg:theme(colors.indigo.600)] [--chkfg:orange] checked:border-indigo-800"
+							/>
+							<p>
+								Saya yakin untuk{" "}
+								<span className="italic font-bold">
+									memvalidasi hafalan surat
+								</span>{" "}
+								mahasiswa tersebut.
+							</p>
+						</div>
 
-            <div className="modal-action">
-              <button
-                className={`w-1/2 btn btn-rounded-sm btn-success ${
-                  (!isChecked || isLoading) && "btn-disabled"
-                }`}
-                onClick={handleValidation}
-              >
-                {isLoading ? (
-                  <span className="loading loading-spinner"></span>
-                ) : (
-                  "Validasi"
-                )}
-              </button>
-              <button
-                className="w-1/2 btn btn-rounded-sm btn-error"
-                onClick={() => {
-                  setShowModal(false);
-                  setIsChecked(false);
-                }}
-                disabled={isLoading}
-              >
-                Batalkan
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+						<div className="modal-action">
+							<button
+								className={`w-1/2 btn btn-rounded-sm btn-success ${
+									(!isChecked || isLoading) && "btn-disabled"
+								}`}
+								onClick={handleValidation}
+							>
+								{isLoading ? (
+									<span className="loading loading-spinner"></span>
+								) : (
+									"Validasi"
+								)}
+							</button>
+							<button
+								className="w-1/2 btn btn-rounded-sm btn-error"
+								onClick={() => {
+									setShowModal(false);
+									setIsChecked(false);
+								}}
+								disabled={isLoading}
+							>
+								Batalkan
+							</button>
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default MahasiswaPASetoran;
