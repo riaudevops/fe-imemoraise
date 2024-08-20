7965b1ddc0c3557bba291a40613ef53d089339fd
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-20 20:56:53 +0700

    ✨ perf: adding load state & fix infinity reload

--- src/components/dashboard/mahasiswa/DashboardMahasiswa.tsx
+++ src/components/dashboard/mahasiswa/DashboardMahasiswa.tsx
@@ -5,60 +5,72 @@ import { useEffect, useState } from "react";
 import { labelPersyaratan } from "../Constant";
 
 const DashboardMahasiswa = () => {
-  const axiosInstance = useAxiosInstance();
-  const { keycloak } = useKeycloak();
+	const axiosInstance = useAxiosInstance();
+	const { keycloak } = useKeycloak();
 
-  const [statsInfoSetoranMahasiswa, setStatsInfoSetoranMahasiswa] = useState<
-    statsInfoSetoranMahasiswaProps[]
-  >([]);
+	const [statsInfoSetoranMahasiswa, setStatsInfoSetoranMahasiswa] = useState<
+		statsInfoSetoranMahasiswaProps[]
+	>([]);
 
-  useEffect(() => {
-    axiosInstance
-      .get(
-        `/mahasiswa/setoran/info/${keycloak.tokenParsed?.email.split("@")[0]}`
-      )
-      .then((res) => res.data.data)
-      .then((res) => {
-        setStatsInfoSetoranMahasiswa(res);
-      });
-  }, [axiosInstance, keycloak.tokenParsed?.email]);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  return (
-    <>
-      <p className="mt-2 text-xl font-bold md:text-3xl">
-        🔥 Progress Setoran hafalan mu...
-      </p>
-      <p className="pt-2 pl-2 mb-6 text-sm lg:text-base">
-        Berikut statistik progress dari setoran hafalan kamu untuk persyaratan
-        akademik di UIN Suska Riau, Semangat terus yaa.. ❤💛
-      </p>
+	useEffect(() => {
+		setIsLoading(true);
+		axiosInstance
+			.get(
+				`/mahasiswa/setoran/info/${keycloak.tokenParsed?.email.split("@")[0]}`
+			)
+			.then((res) => res.data.data)
+			.then((res) => {
+				setIsLoading(false);
+				setStatsInfoSetoranMahasiswa(res);
+			});
+	}, [keycloak.tokenParsed?.email]);
 
-      <div className="space-y-6">
-        {statsInfoSetoranMahasiswa?.map(
-          (data: statsInfoSetoranMahasiswaProps) => (
-            <div
-              key={data.label}
-              className="p-6 transition-colors rounded-lg bg-base-200 hover:bg-base-300 hover:skew-y-1"
-            >
-              <p className="mb-4 text-lg font-bold">
-                {labelPersyaratan(data.label)[1]} ({data.persentase})
-              </p>
-              <div className="flex items-center gap-4">
-                <progress
-                  className="flex-grow h-8 progress progress-accent"
-                  value={data.jumlah_sudah_setor}
-                  max={data.jumlah_wajib_setor}
-                />
-                <p className="text-lg italic whitespace-nowrap">
-                  ({data.jumlah_sudah_setor}/{data.jumlah_wajib_setor})
-                </p>
-              </div>
-            </div>
-          )
-        )}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<p className="mt-2 text-xl font-bold md:text-3xl">
+				🔥 Progress Setoran hafalan mu...
+			</p>
+			<p className="pt-2 pl-2 mb-6 text-sm lg:text-base">
+				Berikut statistik progress dari setoran hafalan kamu untuk persyaratan
+				akademik di UIN Suska Riau, Semangat terus yaa.. ❤💛
+			</p>
+
+			{isLoading ? (
+				<div className="w-full h-96 flex justify-center items-center flex-col gap-3">
+					<span className="loading loading-bars loading-lg"></span>
+          <span className="italic">Sebentar mas, data-nya sedang kami jemput...</span>
+          <button className="btn btn-outline btn-primary" onClick={() => window.location.reload()}>kalo belum bisa juga, coba tekan ini mas... 🤞🏻😅</button>
+				</div>
+			) : (
+				<div className="space-y-6">
+					{statsInfoSetoranMahasiswa?.map(
+						(data: statsInfoSetoranMahasiswaProps) => (
+							<div
+								key={data.label}
+								className="p-6 transition-colors rounded-lg bg-base-200 hover:bg-base-300 hover:skew-y-1"
+							>
+								<p className="mb-4 text-lg font-bold">
+									{labelPersyaratan(data.label)[1]} ({data.persentase})
+								</p>
+								<div className="flex items-center gap-4">
+									<progress
+										className="flex-grow h-8 progress progress-accent"
+										value={data.jumlah_sudah_setor}
+										max={data.jumlah_wajib_setor}
+									/>
+									<p className="text-lg italic whitespace-nowrap">
+										({data.jumlah_sudah_setor}/{data.jumlah_wajib_setor})
+									</p>
+								</div>
+							</div>
+						)
+					)}
+				</div>
+			)}
+		</>
+	);
 };
 
 export default DashboardMahasiswa;
