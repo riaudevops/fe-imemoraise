f1bc312a563a7da66b518e7922907303cbcb2056
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-26 11:07:18 +0700

    🔥 feat: adding information alert when session is about to end

--- src/configs/axios.configs.ts
+++ src/configs/axios.configs.ts
@@ -2,55 +2,62 @@ import { useKeycloak } from "@react-keycloak/web";
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 const useAxiosInstance = () => {
-  const { keycloak } = useKeycloak();
+	const { keycloak } = useKeycloak();
 
-  // Buat instance Axios
-  const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-  });
+	// Buat instance Axios
+	const axiosInstance = axios.create({
+		baseURL: import.meta.env.VITE_API_URL,
+	});
 
-  // Request Interceptor untuk menambahkan access token ke header Authorization
-  axiosInstance.interceptors.request.use(
-    async (config: InternalAxiosRequestConfig) => {
-      if (keycloak.token) {
-        const token = keycloak.token;
+	// Request Interceptor untuk menambahkan access token ke header Authorization
+	axiosInstance.interceptors.request.use(
+		async (config: InternalAxiosRequestConfig) => {
+			if (keycloak.token) {
+				const token = keycloak.token;
 
-        // Tambahkan Authorization header
-        config.headers.Authorization = `Bearer ${token}`;
+				// Tambahkan Authorization header
+				config.headers.Authorization = `Bearer ${token}`;
 
-        // Cek apakah token masih valid atau perlu di-refresh
-        const isTokenExpired = keycloak.isTokenExpired(10); // Check if token will expire in 10 seconds
-        if (isTokenExpired) {
-          try {
-            await keycloak.updateToken(30); // Refresh token if it will expire in 30 seconds
-            config.headers.Authorization = `Bearer ${keycloak.token}`;
-          } catch (error) {
-            console.error("Failed to refresh token:", error);
-            keycloak.logout(); // Logout user if token cannot be refreshed
-          }
-        }
-      }
+				// Cek apakah token masih valid atau perlu di-refresh
+				const isTokenExpired = keycloak.isTokenExpired(10); // Check if token will expire in 10 seconds
+				if (isTokenExpired) {
+					try {
+						await keycloak.updateToken(30); // Refresh token if it will expire in 30 seconds
+						config.headers.Authorization = `Bearer ${keycloak.token}`;
+					} catch (error) {
+						console.error("Failed to refresh token:", error);
+						alert(
+							"Oops! sepertinya sesi anda telah habis, silahkan login kembali..."
+						);
+						keycloak.logout(); // Logout user if 401 Unauthorized is received
+					}
+				}
+			}
 
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
+			return config;
+		},
+		(error) => {
+			return Promise.reject(error);
+		}
+	);
 
-  // Response Interceptor (opsional)
-  axiosInstance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      // Handle unauthorized errors or token expiration
-      if (error.response?.status === 401) {
-        keycloak.logout(); // Logout user if 401 Unauthorized is received
-      }
-      return Promise.reject(error);
-    }
-  );
+	// Response Interceptor (opsional)
+	axiosInstance.interceptors.response.use(
+		(response) => response,
+		(error) => {
+			console.log(error);
+			// Handle unauthorized errors or token expiration
+			if (error.response?.status === 401) {
+				alert(
+					"Oops! sepertinya sesi anda telah habis, silahkan login kembali..."
+				);
+				keycloak.logout(); // Logout user if 401 Unauthorized is received
+			}
+			return Promise.reject(error);
+		}
+	);
 
-  return axiosInstance;
+	return axiosInstance;
 };
 
 export default useAxiosInstance;
