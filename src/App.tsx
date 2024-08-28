635c1e3d4d9c0ff36429f2622d6868537b5192ed
MFarhanZ1 <farhanaulia.p@gmail.com>
2024-08-28 14:12:19 +0700

    ✨ perf: adjust env var name sync with Dockerfile acceptance var

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routers/app.routers";
 
 const keycloak = new Keycloak({
-  url: import.meta.env.VITE_KEYCLOAK_URL,
-  realm: import.meta.env.VITE_KEYCLOAK_REALM,
-  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT,
+  url: import.meta.env.VITE_KEYCLOAK_URL_PLACEHOLDER,
+  realm: import.meta.env.VITE_KEYCLOAK_REALM_PLACEHOLDER,
+  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_PLACEHOLDER,
 });
 
 const App = () => {
